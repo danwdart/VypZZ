a58aa1648712097dbaf99262502ca629e3eba0e6
Dan Dart <dandart@googlemail.com>
2013-11-12 00:14:28 +0000

    Start to the CSS structure

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+	cons = require('consolidate'),
+	app = express();
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+app.use(express.compress());
+app.use(express.cookieParser('my secret here'));
+app.use(express.static('public'));
+
+app.engine('mustache', cons.mustache);
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/templates');	
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000);
